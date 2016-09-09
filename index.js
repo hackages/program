89b182c7b8078bd912286ea500cab21abe7bce95
davyengone <engonenzedavy@yahoo.com>
2016-09-09 17:42:41 +0200

    added first element

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import React from 'react';
+import React, { DOM, Component } from 'react';
+import { render } from 'react-dom';
+
+class Main extends Component {
+  render (){
+    return (
+      DOM.h1(null, "hi hao, React")
+    );
+  }
+}
+
+const domElement = document.getElementById('program');
+
+render(<Main/>, domElement);
+
